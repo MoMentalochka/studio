4af06adb159d6adf97be90f3f45279163c2e8be8
MoMentalochka <56727108+MoMentalochka@users.noreply.github.com>
2019-12-09 18:26:34 +0300

    ff

---
+++ vendor/js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('.toggle').click(function(){
+        var get_id = this.id;
+        var get_current = $('.info.'+get_id);
+        $('.info').not(get_current).hide();
+       get_current.show();
+    });
+   
+});
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items : 2,
+        loop : true,
+        nav: true
+    });
+  });
